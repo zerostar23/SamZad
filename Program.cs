6d12abeb4b966cff43e23d395086a250c540e751
Georgiy.P <killerkip07@mail.ru>
2021-12-27 19:13:58 +0300

    Добавил аргументы для метода FillArray

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 ﻿
-int[] FillArray()
+int[] FillArray(int lenA, int minRan, int maxRan)
 {
-    int[] ArrayA = new int[10];
+    int[] ArrayA = new int[lenA];
     for (int i = 0; i < ArrayA.Length; i++)
     {
-        ArrayA[i] = new Random().Next(-10, 20);
+        ArrayA[i] = new Random().Next(minRan, maxRan);
     }
-    return ArrayA;
+    return ArrayA; 
 }
 
 int[] Dev1(int[] ArrayA)
@@ -14,33 +14,13 @@ int[] Dev1(int[] ArrayA)
     int lenB = 0;
     for (int i = 0; i < ArrayA.Length; i++)
     {
-        if (ArrayA[i] %2 == 0) lenB++; 
+        if (ArrayA[i] %2 == 0 && ArrayA[i] != 0) lenB++; 
     }
     int[] ArrayB = new int[lenB];
     int j = 0;
     for (int i = 0; i < ArrayA.Length; i++)
     {
-        if (ArrayA[i] %2==0)
-        {
-            ArrayB[j] = ArrayA[i];
-            j++;
-        }
-    }
-    return (ArrayB); 
-}
-
-int[] Dev2(int[] ArrayA)
-{
-    int lenB = 0;
-    for (int i = 0; i < ArrayA.Length; i++)
-    {
-        if (ArrayA[i] > 8) lenB++; 
-    }
-    int[] ArrayB = new int[lenB];
-    int j = 0;
-    for (int i = 0; i < ArrayA.Length; i++)
-    {
-        if (ArrayA[i] > 8)
+        if (ArrayA[i] %2 == 0 && ArrayA[i] != 0)
         {
             ArrayB[j] = ArrayA[i];
             j++;
@@ -74,9 +54,8 @@ void WriteArray(int[] ArrayA, int[] ArrayB)
 
 void Task()
 {
-    int[] ArrayA = FillArray();
+    int[] ArrayA = FillArray(15, -5, 15);
     WriteArray(ArrayA, Dev1(ArrayA));
-    WriteArray(ArrayA, Dev2(ArrayA));
 }
 
 Task();
